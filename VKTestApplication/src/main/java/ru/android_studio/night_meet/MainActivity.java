bb8ac09bf08a30341dac377f47a8df3dc38efadc
Yury Andreev <andreevym@gmail.com>
2016-08-28 22:01:29 +0300

    add left menu

--- VKTestApplication/src/main/java/ru/android_studio/night_meet/MainActivity.java
+++ VKTestApplication/src/main/java/ru/android_studio/night_meet/MainActivity.java
@@ -2,10 +2,16 @@ package ru.android_studio.night_meet;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -21,7 +27,8 @@ import ru.android_studio.night_meet.retrofit.api.VkAPI;
 import ru.android_studio.night_meet.retrofit.model.User;
 import ru.android_studio.night_meet.retrofit.model.Users;
 
-public class MainActivity extends AppCompatActivity implements Callback<Users> {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, Callback<Users> {
 
     private static final String TAG = "MainActivity";
 
@@ -34,24 +41,6 @@ public class MainActivity extends AppCompatActivity implements Callback<Users> {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        /*profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
-        greeting = (TextView) findViewById(R.id.greeting);
-
-        postStatusUpdateButton = (Button) findViewById(R.id.postStatusUpdateButton);
-        postStatusUpdateButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                onClickPostStatusUpdate();
-            }
-        });
-
-        postPhotoButton = (Button) findViewById(R.id.postPhotoButton);
-        postPhotoButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                onClickPostPhoto();
-            }
-        });*/
-
         init();
 
         try {
@@ -60,9 +49,6 @@ public class MainActivity extends AppCompatActivity implements Callback<Users> {
             Log.e(TAG, e.getLocalizedMessage());
             e.printStackTrace();
         }
-
-        Toolbar toolbar = (Toolbar) findViewById(ru.android_studio.night_meet.R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton next = (FloatingActionButton) findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
@@ -92,6 +78,18 @@ public class MainActivity extends AppCompatActivity implements Callback<Users> {
                 Snackbar.make(view, "Like", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void init() {
@@ -124,5 +122,62 @@ public class MainActivity extends AppCompatActivity implements Callback<Users> {
     public void onFailure(Call<Users> call, Throwable t) {
         Log.e(TAG, t.getLocalizedMessage());
         t.printStackTrace();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
